76d205ed225cc6436c30b42cca390aa969a64c81
Ansel <jake.madden@hotmail.co.uk>
2018-11-20 20:22:26 +0000

    Update EZ file redirection tool for linux.sh

--- EZ file redirection tool for linux.sh
+++ EZ file redirection tool for linux.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
-# EZ file redirection tool for linux
-
+# EZ file redirection tool for Linux
 #functions
+INPUT=/tmp/menu.sh.$$
+# Storage file for displaying cal and date command output
+OUTPUT=/tmp/output.sh.$$
+# get text editor or fall back to vi_editor
+vi_editor=${EDITOR-vi}
+# trap and delete temp files
+trap "rm $OUTPUT; rm $INPUT; exit" SIGHUP SIGINT SIGTERM
 function display_output(){
-	local h=${1-10}			# box height default 10
-	local w=${2-41} 		# box width default 41
-	local t=${3-Output} 	# box title 
-	dialog --backtitle "EZ file redirection tool for linux" --title "${t}" --clear --msgbox "$(<$OUTPUT)" ${h} ${w}
+        local h=${1-10}         # box height default 10
+        local w=${2-41}         # box width default 41
+        local t=${3-Output}     # box title
+        dialog --backtitle "EZ file redirection tool for Linux" --title "${t}" --clear --msgbo$
 }
-function named.local{
-	$nano /etc/bind/named.conf.local
+function named.local(){
+        nano /etc/bind/named.conf.local
 }
-function named.options{
-	$nano /etc/bind/named.conf.optionss
+
+function named.options(){
+        nano /etc/bind/named.conf.options
 }
-function open.cheese.db{
-	$nano /etc/bind/zones/cheese.db
+function open.cheese.db(){
+        nano /etc/bind/zones/cheese.db
 }
 # infinite loop
-while true
-do
-dialog --clear --help-button --backtitle "EZ file redirection tool for linux" \
---title "[ M A I N - M E N U ]" \
---menu "You can use the UP/DOWN arrow keys, the first \n\
-letter of the choice as a hot key, or the \n\
-number keys 1-9 to choose an option.\n\
+#while true
+#do
+dialog --clear --help-button --backtitle "EZ file redirection tool for Linux" \
+--title "[Main Menu]" \
+--menu "You can use the UP/DOWN arrow keys\n\
 Choose the TASK" 15 50 4 \
-.local "Opens named.conf.local" \
-.options "Opens named.conf.options" \
+Local "Opens named.conf.local" \
+Options "Opens named.conf.options" \
 Cheese.db "Opens cheeseZone.db" \
 Exit "Exit to the shell" 2>"${INPUT}"
 
 menuitem=$(<"${INPUT}")
-# make decsion 
+
 case $menuitem in
-	.local) named.local;;
-	.options) named.option;;
-	cheese.db) open.cheese.db;;
-	Exit) echo "Bye"; break;;s
+        Local) named.local;;
+        Options) named.options;;
+        Cheese.db) open.cheese.db;;
+        Exit) echo "Bye";break;;
 esac
+
 done
+# if temp files found, delete em
+[ -f $OUTPUT ] && rm $OUTPUT
+[ -f $INPUT ] && rm $INPUT
